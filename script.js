c046305d8b8f01bd1c72d5247fb6f4bfefbc83f6
highcl0uds <highclouds@ya.ru>
2021-06-28 02:31:26 +0300

    nav menu and started main sections

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+"use strict";
+const headerBurger = document.querySelector(".header__burger");
+const headerMenu = document.querySelector(".header__menu");
 
+console.log(headerBurger);
+
+headerBurger.addEventListener("click", () => {
+  headerBurger.classList.toggle("active");
+  headerMenu.classList.toggle("active");
+  document.body.classList.toggle("lock");
+});
