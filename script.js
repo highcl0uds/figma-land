7cb7621875b2c710f2bb0e00412fd2a1b3623916
highcl0uds <highclouds@ya.ru>
2021-07-02 10:50:24 +0300

    commit 02.07.2021

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-"use strict";
-const headerBurger = document.querySelector(".header__burger");
-const headerMenu = document.querySelector(".header__menu");
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+});
+const headerBurger = document.querySelector('.header__burger');
+const headerMenu = document.querySelector('.header__menu');
+const menuBacktocontent = document.querySelector('.menu__backtocontent');
 
 console.log(headerBurger);
 
-headerBurger.addEventListener("click", () => {
-  headerBurger.classList.toggle("active");
-  headerMenu.classList.toggle("active");
-  document.body.classList.toggle("lock");
+headerBurger.addEventListener('click', () => {
+  headerBurger.classList.toggle('active');
+  headerMenu.classList.toggle('active');
+  document.body.classList.toggle('lock');
+});
+
+menuBacktocontent.addEventListener('click', () => {
+  headerMenu.classList.toggle('active');
+  document.body.classList.toggle('lock');
 });
